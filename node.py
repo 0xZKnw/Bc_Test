93d4cd2290bb6bea068596f0107b3ba740dcd1a3
0xNyFime <91630821+NyFime@users.noreply.github.com>
2024-11-13 12:30:20 +0100

    Update node.py

--- node.py
+++ node.py
@@ -16,11 +16,11 @@ class Node:
         server.listen(5)
         print(f"Écoute sur {self.host}:{self.port}")
 
-        while True:
-            conn, addr = server.accept()
-            print(f"Connexion acceptée de {addr}")
-            threading.Thread(target=self.handleClient, args=(conn, addr)).start()
-            self.connect_to_peer(addr[0], 5006)
+        #while True:
+        conn, addr = server.accept()
+        print(f"Connexion acceptée de {addr}")
+        threading.Thread(target=self.handleClient, args=(conn, addr)).start()
+        self.connect_to_peer(addr[0], 5006)
 
     def handleClient(self, conn, addr):
         '''ecoute message entrant et repond'''
@@ -42,10 +42,11 @@ class Node:
         '''connection aux appareils entrant dans le reseau'''
         peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            peer.connect((host, port))
-            self.peers.append(peer)
-            print(f"Connecté au pair {host}:{port}")
-            peer.sendall(b"bite")
+            if peer not in self.peers:
+                peer.connect((host, port))
+                self.peers.append(peer)
+                print(f"Connecté au pair {host}:{port}")
+                peer.sendall(b"bite")
         except Exception as e:
             print(f"Erreur lors de la connexion au pair {host}:{port} : {e}")
 
@@ -60,6 +61,6 @@ class Node:
 node_1 = Node('192.168.173.185', 5006)
 server_thread = threading.Thread(target=node_1.start_server)
 server_thread.start()
-while a != "STOP":
+while True:
     a = str(input(""))
     node_1.broadcast_message(a)
