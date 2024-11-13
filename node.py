51af309721c0c60fc10a08e7ec8751814ebc1dcd
0xNyFime <91630821+NyFime@users.noreply.github.com>
2024-11-13 12:18:05 +0100

    Update node.py

--- node.py
+++ node.py
@@ -8,7 +8,7 @@ class Node:
         self.port = port
         self.peers = []
     
-    def start_server(self, pseudo):
+    def start_server(self):
         '''demarrage du serveur'''
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -20,7 +20,7 @@ class Node:
             conn, addr = server.accept()
             print(f"Connexion acceptée de {addr}")
             threading.Thread(target=self.handleClient, args=(conn, addr)).start()
-            self.connect_to_peer(addr[0], 5006, pseudo)
+            self.connect_to_peer(addr[0], 5006)
 
     def handleClient(self, conn, addr):
         '''ecoute message entrant et repond'''
@@ -31,20 +31,21 @@ class Node:
                 if not data:
                     break
                 print(f"Message reçu de {addr}: {data.decode('utf-8')}")
+                conn.sendall(b"recu")
         except Exception as e:
             print(f"Erreur avec {addr}: {e}")
         finally:
             conn.close()
             print(f"Connexion fermée avec {addr}")
 
-    def connect_to_peer(self, host, port, pseudo):
+    def connect_to_peer(self, host, port):
         '''connection aux appareils entrant dans le reseau'''
         peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             peer.connect((host, port))
             self.peers.append(peer)
             print(f"Connecté au pair {host}:{port}")
-            peer.sendall(b"psdo:" + pseudo.encode('utf-8'))
+            peer.sendall(b"bite")
         except Exception as e:
             print(f"Erreur lors de la connexion au pair {host}:{port} : {e}")
 
@@ -55,17 +56,10 @@ class Node:
                 peer.sendall(message.encode('utf-8'))
             except Exception as e:
                 print(f"Erreur lors de l'envoi au pair : {e}")
-    def close(self):
-        self.close()
 
-ipLocal = str(input("ton ip ? : "))
-pseudo = str(input("ton pseudo : "))
-a = f"{pseudo} : "
-node_1 = Node(ipLocal, 5006)
-server_thread = threading.Thread(target=node_1.start_server, args=(pseudo,))
+node_1 = Node('192.168.173.185', 5006)
+server_thread = threading.Thread(target=node_1.start_server)
 server_thread.start()
 while a != "STOP":
-    a = f"{pseudo} : "
-    a += str(input(""))
+    a = str(input(""))
     node_1.broadcast_message(a)
-node_1.close()
